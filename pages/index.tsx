86f40c2a6362d38c79ed97f1ec872c3643ab263a
makom <mako.mail.g@gmail.com>
2022-07-28 08:56:38 +0900

    fix layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,30 @@ import Layout from '../components/layout';
 const Home: FC = () => (
   <Layout>
     <h1 className={styles.title}>makowis.com</h1>
-    <p>ポートフォリオサイトという名のリンク集</p>
-    <ul className={styles.links}>
-      <li>
-        <a href="https://twitter.com/mako_wis">Twitter</a>
-      </li>
-      <li>
-        <a href="https://www.facebook.com/makoto.henmi">Facebook</a>
-      </li>
-      <li>
-        <a href="https://www.instagram.com/mako_wis/">Instagram</a>
-      </li>
-      <li>
-        <a href="https://github.com/makowis">GitHub</a>
-      </li>
-      <li>
-        <a href="https://mako-wis.hatenablog.com/">ブログ：makoラボ</a>
-      </li>
-      <li>
-        <a href="https://speakerdeck.com/makowis">Speaker Deck</a>
-      </li>
-    </ul>
+    <p className={styles.description}>ポートフォリオサイトという名のリンク集</p>
+    <section className={styles.subSection}>
+      <h2 className={styles.subTitle}>Links</h2>
+      <ul className={styles.links}>
+        <li>
+          <a href="https://twitter.com/mako_wis">Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/makoto.henmi">Facebook</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/mako_wis/">Instagram</a>
+        </li>
+        <li>
+          <a href="https://github.com/makowis">GitHub</a>
+        </li>
+        <li>
+          <a href="https://speakerdeck.com/makowis">Speaker Deck</a>
+        </li>
+        <li>
+          <a href="https://mako-wis.hatenablog.com/">Blog</a>
+        </li>
+      </ul>
+    </section>
   </Layout>
 );
 
