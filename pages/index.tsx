f715e61501fdc54cd77112da24c0a62817a93ce4
makom <mako.mail.g@gmail.com>
2022-07-27 20:27:16 +0900

    change home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,28 @@ import Layout from '../components/layout';
 
 const Home: FC = () => (
   <Layout>
-    <h1 className={styles.title}>
-      Welcome to <a href="https://nextjs.org">Next.js!</a>
-    </h1>
-
-    <p className={styles.description}>
-      Get started by editing <code className={styles.code}>pages/index.js</code>
-    </p>
+    <h1 className={styles.title}>makowis.com</h1>
+    <p>ポートフォリオサイトという名のリンク集</p>
+    <ul className={styles.links}>
+      <li>
+        <a href="https://twitter.com/mako_wis">Twitter</a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/makoto.henmi">Facebook</a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/mako_wis/">Instagram</a>
+      </li>
+      <li>
+        <a href="https://github.com/makowis">GitHub</a>
+      </li>
+      <li>
+        <a href="https://mako-wis.hatenablog.com/">ブログ：makoラボ</a>
+      </li>
+      <li>
+        <a href="https://speakerdeck.com/makowis">Speaker Deck</a>
+      </li>
+    </ul>
   </Layout>
 );
 
