7a70fc98e5cb58fb920e0cd42b1d910fe5af8ba8
makowis <makowis@users.noreply.github.com>
2022-07-28 09:26:43 +0000

    deploy: 68d258b65231d03ae8d58723a4b0536edebfb319

--- _next/static/4VOrqU_hOEZD4UPigyxYT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fc0b628e46a803b2.css","static/chunks/pages/index-afc79cc0f793f1bc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/cA7bM-T0F71r9iuPxGK8e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fc0b628e46a803b2.css","static/chunks/pages/index-d4546bcbc1beb2c1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
