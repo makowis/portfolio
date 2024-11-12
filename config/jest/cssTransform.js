bd47f44102b3c15db42fd16fd2bae87179f29d00
makom <mako.mail.g@gmail.com>
2024-11-12 16:21:40 +0900

    fix jest error css Transform
    Jest28 からproccesのメソッドの戻り値が変更されたので修正
    https://jestjs.io/docs/code-transformation

--- config/jest/cssTransform.js
+++ config/jest/cssTransform.js
@@ -1,6 +1,8 @@
 module.exports = {
-  process() {
-    return 'module.exports = {};';
+  process() {    
+    return {
+      code: 'module.exports = {};'
+    };
   },
   getCacheKey() {
     return 'cssTransform';
