e7a9804a82c98ecf6d67d9723e48b01a5cf889c7
makom <mako.mail.g@gmail.com>
2024-11-12 15:02:57 +0900

    fix test setting

--- jest.config.js
+++ jest.config.js
@@ -11,18 +11,10 @@ module.exports = {
     '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  // setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': ['ts-jest',{
+      tsconfig: '<rootDir>/test/tsconfig.jest.json',
+    }],
     '^.+\\.(css|scss)$': '<rootDir>/config/jest/cssTransform.js',
-  },
-  // https://github.com/zeit/next.js/issues/8663#issue-490553899
-  globals: {
-    // we must specify a custom tsconfig for tests because we need the typescript transform
-    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
-    // can see this setting in tsconfig.jest.json -> "jsx": "react"
-    'ts-jest': {
-      tsConfig: '<rootDir>/test/tsconfig.jest.json',
-    },
   },
 };
