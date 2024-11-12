a115328c7b7c4913f87167f012ab57e6aab9f8a4
makom <mako.mail.g@gmail.com>
2024-11-12 14:46:03 +0900

    fix lint

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ['**/out', '**/.next', '**/.idea'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,28 @@
 module.exports = {
   collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/*.d.ts",
-    "!**/node_modules/**",
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
   ],
   preset: 'ts-jest',
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   // setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-    "^.+\\.(css|scss)$": "<rootDir>/config/jest/cssTransform.js"
+    '^.+\\.(css|scss)$': '<rootDir>/config/jest/cssTransform.js',
   },
-   // https://github.com/zeit/next.js/issues/8663#issue-490553899
+  // https://github.com/zeit/next.js/issues/8663#issue-490553899
   globals: {
     // we must specify a custom tsconfig for tests because we need the typescript transform
     // to transform jsx into js rather than leaving it jsx such as the next build requires. you
     // can see this setting in tsconfig.jest.json -> "jsx": "react"
     'ts-jest': {
-      'tsConfig': '<rootDir>/test/tsconfig.jest.json'
-    }
-  }
-}
+      tsConfig: '<rootDir>/test/tsconfig.jest.json',
+    },
+  },
+};
