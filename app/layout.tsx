942f632cdec0a119b696e56b5f0abe7b81a9f4a6
makowis <mako.mail.g@gmail.com>
2025-01-22 16:03:05 +0900

    fix

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import Footer from './_layout/footer';
+import styles from '../styles/layout.module.scss';
+import '../styles/globals.css';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+  title: 'mako_wis.dev',
+  description: 'mako_wisのポートフォリオサイト',
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <html lang='ja'>
+      <body>
+        <div className={styles.container}>
+          <main className={styles.main}>{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  )
+}  
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Home from '../../pages/index';
+import Home from '../../app/page';
 
 it('renders correctly', () => {
   const { container } = render(<Home />);
